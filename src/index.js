de7dc2bdae20580ff7331107c17896dc945646ac
Mandeep Kundal <mk.kundal@gmail.com>
2024-01-25 02:24:15 -0500

    added changes to run server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import connectDb from "./db/index.js";
 import "dotenv/config";
+import { app } from "./app.js";
 
-connectDb();
+const port = process.env.PORT || 8000;
+
+connectDb()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running at port: ${port}`);
+        })
+    })
+    .catch((err) => {
+        console.log("Connection to mongodb failed:", err);
+    });
