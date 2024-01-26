9ed34668192714dc46d9a45cb86885096093e751
Mandeep Kundal <mk.kundal@gmail.com>
2024-01-26 15:58:55 -0500

    Added missing await keyword

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -50,7 +50,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function(next) {
     if (!this.isModified("password")) return next();
     // Only encrypt password when password field is modified
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
