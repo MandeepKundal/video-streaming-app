8add1598d34a388951ed2e266bf6fd05fb0029b9
Mandeep Kundal <mk.kundal@gmail.com>
2024-01-26 16:05:25 -0500

    Added controller for User Registration

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js"
 
 const userRouter = Router();
-userRouter.route("/register").post(registerUser); // http://localhost:8000/api/v1/users/register
+userRouter.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser // http://localhost:8000/api/v1/users/register
+);
 
 export default userRouter;
